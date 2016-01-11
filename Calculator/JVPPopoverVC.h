d7b6f277666f5c9c573ed22543e0bd6094bb1e17
Julia Vashchenko <aronskaya.yulia@gmail.com>
2016-01-11 18:39:14 +0200

    Adds JVPPopoverVC.h and .m files, slightly updates the storyboard

---
+++ Calculator/JVPPopoverVC.h
@@ -0,0 +1,13 @@
+//
+//  JVPPopoverVC.h
+//  CalculatorFree
+//
+//  Created by Julia Vashchenko on 1/9/16.
+//  Copyright © 2016 Julia Vashchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVPPopoverVC : UIViewController
+
+@end
